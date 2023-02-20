753de0ed743342de0c2c05a9ebffd2662ac3a86f
Harry Shindika <harry.shindika@yale.edu>
2023-02-20 13:09:43 -0500

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// added to reduce import
+import { render } from 'react-dom';
 import App from './App';
 import './index.css'
 import {BrowserRouter as Router} from 'react-router-dom'
